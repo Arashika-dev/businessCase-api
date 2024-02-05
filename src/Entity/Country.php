9d94b942fd6f125c7f0abed552d6365457d78302
Arashika-First <morelalessi.florian@gmail.com>
2024-02-05 22:11:47 +0100

    api-platform, country requests

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -2,20 +2,44 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
 use App\Repository\CountryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[ORM\Entity(repositoryClass: CountryRepository::class)]
+#[ApiResource(
+    operations:[
+        new Get(normalizationContext: ['groups' => ['country:item']]),
+        new GetCollection(normalizationContext: ['groups' => ['country:list']]),
+        new Delete(normalizationContext: ['groups' => ['country:delete']]),
+        new Post(normalizationContext: ['groups' => ['country:create']]),
+        new Patch(normalizationContext: ['groups' => ['country:modify']]),
+
+    ]
+)]
+#[ApiFilter(SearchFilter::class, properties: ['name' => 'ipartial'])]
 class Country
 {
     #[ORM\Id]
     #[ORM\Column(length:3)]
+    #[Groups('country:item', 'country:list', 'country:delete', 'country:create', 'country:modify')]
     private ?string $code = null;
 
 
     #[ORM\Column(length: 255, unique:true)]
+    #[Groups('country:item', 'country:list', 'country:delete', 'country:create', 'country:modify')]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'country', targetEntity: City::class, orphanRemoval: true)]
